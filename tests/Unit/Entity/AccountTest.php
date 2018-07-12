4f78620ddfa95aef8a4313bbe13f2513a233251a
PawelPodkalicki <ppodkali@gmail.com>
2018-07-12 14:34:02 +0200

    Add unit test entities

---
+++ tests/Unit/Entity/AccountTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Adshares\Ads\Tests\Unit\Entity;
+
+use Adshares\Ads\Entity\Account;
+use Adshares\Ads\Entity\EntityFactory;
+
+class AccountTest extends \PHPUnit\Framework\TestCase
+{
+    public function testCreateFromRow(): void
+    {
+        /* @var Account $account */
+        $account = EntityFactory::createAccount($this->getRawData());
+
+        $this->assertEquals('0001-00000000-9B6F', $account->getAddress());
+        $this->assertEquals(1, $account->getNode());
+        $this->assertEquals(0, $account->getId());
+        $this->assertEquals(4, $account->getMsid());
+        $this->assertEquals(new \DateTime('@1531394984'), $account->getTime());
+        $this->assertEquals(1, $account->getPairedNode());
+        $this->assertEquals(0, $account->getPairedId());
+        $this->assertEquals(7, $account->getStatus());
+        $this->assertEquals(new \DateTime('@1531394976'), $account->getLocalChange());
+        $this->assertEquals(new \DateTime('@1531396672'), $account->getRemoteChange());
+        $this->assertEquals(1999999999743316130, $account->getBalance());
+        $this->assertEquals(
+            'A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363',
+            $account->getPublicKey()
+        );
+        $this->assertEquals('811F420FDC7FA662BE1A0B7295C88BAD3307C337129A1A52D9A1598FD8486009', $account->getHash());
+    }
+
+    private function getRawData(): array
+    {
+        return json_decode('{
+            "address": "0001-00000000-9B6F",
+            "node": "1",
+            "id": "0",
+            "msid": "4",
+            "time": "1531394984",
+            "date": "2018-07-12 11:29:44",
+            "status": "7",
+            "paired_node": "1",
+            "paired_id": "0",
+            "local_change": "1531394976",
+            "remote_change": "1531396672",
+            "balance": "19999999.99743316130",
+            "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+            "hash": "811F420FDC7FA662BE1A0B7295C88BAD3307C337129A1A52D9A1598FD8486009"
+        }', true);
+    }
+}
