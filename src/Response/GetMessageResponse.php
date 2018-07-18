302f40771d5d5628467b91a7adf4c139ebfe6994
PawelPodkalicki <ppodkali@gmail.com>
2018-07-18 13:58:46 +0200

    Add message entity

--- src/Response/GetMessageResponse.php
+++ src/Response/GetMessageResponse.php
@@ -3,9 +3,15 @@
 namespace Adshares\Ads\Response;
 
 use Adshares\Ads\Entity\EntityFactory;
+use Adshares\Ads\Entity\Message;
 
 class GetMessageResponse extends AbstractResponse
 {
+    /**
+     * @var Message
+     */
+    protected $message;
+
     /**
      * @var array[AbstractTransaction]
      */
@@ -18,11 +24,21 @@ class GetMessageResponse extends AbstractResponse
     {
         parent::loadData($data);
 
+        $this->message = EntityFactory::createMessage($data);
+
         if (array_key_exists('transactions', $data)) {
             foreach ($data['transactions'] as $value) {
                 $this->transactions[] = EntityFactory::createTransaction($value);
             }
         }
+    }
+
+    /**
+     * @return Message
+     */
+    public function getMessage(): Message
+    {
+        return $this->message;
     }
 
     /**
